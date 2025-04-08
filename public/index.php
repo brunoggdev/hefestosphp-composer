ddf97d3905044cd37f8cfdfffa0a0fa6bb96b33a
Bruno Gomes <brunoggdev@gmail.com>
2025-04-08 17:31:40 -0300

    feat: separando core composer

--- public/index.php
+++ public/index.php
@@ -4,6 +4,6 @@
  Configura todo o necessário para iniciar o app, como buscar as constantes, 
  carregar as funções auxiliares e registrar o autoloader adequado;
 ------------------------------------------------------------------------ */
-require __DIR__.'/../system/iniciar_app.php';
+require __DIR__.'/../vendor/brunoggdev/hefestosphp-core/src/system/iniciar_app.php';
 
 Hefestos\Core\App::processarRequisicao();
